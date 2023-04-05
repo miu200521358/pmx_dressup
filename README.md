66f9eb10184f6c0312d957d52bf9d2bbce34f5ec
miu <garnet200521358@gmail.com>
2023-04-06 02:38:07 +0900

    多言語対応

--- README.md
+++ README.md
@@ -19,3 +19,13 @@
 
  https://icon-rainbow.com/%E3%83%AF%E3%83%B3%E3%83%94%E3%83%BC%E3%82%B9%E3%81%AE%E7%84%A1%E6%96%99%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B3%E7%B4%A0%E6%9D%90-2/
 
+### 翻訳
+
+ 1. potファイルを作成
+    - `pybabel extract -o src/i18n/messages.pot .`
+ 1. poファイルを言語別に作成
+    - `pybabel init -i src/i18n/messages.pot -l ja -d ja -o src/i18n/ja/messages.po`
+    - `pybabel init -i src/i18n/messages.pot -l en -d en -o src/i18n/en-us/messages.po`
+    - `pybabel init -i src/i18n/messages.pot -l zh -d zh -o src/i18n/zh/messages.po`
+    - `pybabel init -i src/i18n/messages.pot -l ko -d ko -o src/i18n/ko/messages.po`
+ 1. translate.batを実行
--- src/executor.py
+++ src/executor.py
@@ -2,10 +2,12 @@ import argparse
 from multiprocessing import freeze_support
 import numpy as np
 import wx
-from mlib.utils.file_utils import get_path, get_root_dir
+import os
+from mlib.utils.file_utils import get_path
 from form.main_frame import MainFrame
 from mlib.base.logger import MLogger
 from mlib.base.logger import LoggingMode
+
 
 VERSION_NAME = "ver1.00.00_β01"
 
@@ -22,11 +24,11 @@ if __name__ == "__main__":
     parser.add_argument("--log_mode", default=0, type=int)
     parser.add_argument("--out_log", default=0, type=int)
     parser.add_argument("--is_saving", default=1, type=int)
-    parser.add_argument("--lang", default="ja_JP", type=str)
+    parser.add_argument("--lang", default="ja", type=str)
     args = parser.parse_args()
 
     # ロガーの初期化
-    MLogger.initialize(args.lang, get_root_dir(), LoggingMode(args.log_mode), level=args.verbose)
+    MLogger.initialize(args.lang, os.path.dirname(os.path.abspath(__file__)), LoggingMode(args.log_mode), level=args.verbose)
 
     # アプリの起動
     app = wx.App(False)
