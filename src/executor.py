06847108f91e02ebb3cf928662ca07f61b8cd0c1
miu <garnet200521358@gmail.com>
2023-04-16 19:59:42 +0900

    PmxDressup_1.00.00_β08 (2023/04/15)
    ・材質の透過度を個別に変更できる機能追加
    ・人物モデルと衣装モデルのボーンの角度差を加味したボーンフィッティング処理に修正
    　・必須ボーンチェック追加（センター、上半身、下半身）
    　・準標準ボーンの範囲外の場合、とりあえず縮尺対象外
    　・モデルによっては変形が汚くなるので、頂点スムージング検討中
    ・描画に失敗した時のエラーメッセージ表示
    ・処理完了時のサウンド
    ・モーションのみを切り替えた場合、人物・衣装のフィッティングはそのままで切り替えられるよう機能追加

--- src/executor.py
+++ src/executor.py
@@ -1,6 +1,5 @@
 import argparse
 import os
-from multiprocessing import freeze_support
 
 import numpy as np
 import wx
@@ -9,13 +8,10 @@ from mlib.base.logger import LoggingMode, MLogger
 from mlib.utils.file_utils import get_path
 
 APP_NAME = "PmxDressup"
-VERSION_NAME = "1.00.00_β07"
+VERSION_NAME = "1.00.00_β08"
 
 # 指数表記なし、有効小数点桁数6、30を超えると省略あり、一行の文字数200
 np.set_printoptions(suppress=True, precision=6, threshold=30, linewidth=200)
-
-# Windowsマルチプロセス対策
-freeze_support()
 
 if __name__ == "__main__":
     # 引数の取得
