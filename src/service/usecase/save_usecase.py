d9f2f935d8f4d753d574127d23867826596913df
miu <garnet200521358@gmail.com>
2023-07-08 21:16:48 +0900

    領域毎の調整

--- src/service/usecase/save_usecase.py
+++ src/service/usecase/save_usecase.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from datetime import datetime
+from itertools import product
 import os
 import shutil
 from typing import Optional
@@ -959,20 +960,57 @@ class SaveUsecase:
         logger.info("肌テクスチャ色補正", decoration=MLogger.Decoration.LINE)
         dress.update_vertices_by_material()
 
-        logger.info("人物(補正元)材質: {m} -> 衣装(補正先)材質: {d}", m=model_material.name, d=dress_material.name)
-
         model_texture = model.textures[model_material.texture_index]
         dress_texture = dress.textures[dress_material.texture_index]
 
+        logger.info("人物(補正元)材質: {m} -> 衣装(補正先)材質: {d}", m=model_material.name, d=dress_material.name)
+
         model_image = np.array(model_texture.image, np.float64)
         dress_image = np.copy(np.array(dress_texture.image, np.float64))
-
         # 補正衣装画像
         corrected_dress_image = np.asarray(np.copy(dress_image))
+
         # 人物の指定材質に割り当てられた頂点INDEXリスト
         model_vertex_indexes = model.vertices_by_materials[model_material.index]
         # 衣装の指定材質に割り当てられた頂点INDEXリスト
         dress_vertex_indexes = dress.vertices_by_materials[dress_material.index]
+
+        # 人物の指定材質に割り当てられた面INDEXリスト
+        model_face_indexes = model.faces_by_materials[model_material.index]
+        # 衣装の指定材質に割り当てられた面INDEXリスト
+        dress_face_indexes = dress.faces_by_materials[dress_material.index]
+
+        logger.info("人物: 近似頂点選別")
+
+        # 人物の頂点に紐付く面の構成頂点INDEXがmodel_vertex_indexesの何番目に存在しているかのリスト
+        model_near_vertices: dict[int, list[int]] = {}
+        for i, model_face_index in enumerate(model_face_indexes):
+            logger.count("人物: 近似頂点選別", i, len(model_face_indexes), display_block=100)
+
+            for vertex_index in model.faces[model_face_index].vertices:
+                if vertex_index not in model_near_vertices:
+                    model_near_vertices[vertex_index] = []
+                for near_vertex_index in model.faces[model_face_index].vertices:
+                    if near_vertex_index in model_vertex_indexes:
+                        model_near_vertices[vertex_index].append(
+                            [vi for (vi, vidx) in enumerate(model_vertex_indexes) if vidx == near_vertex_index][0]
+                        )
+
+        logger.info("衣装: 近似頂点選別")
+
+        # 衣装の頂点に紐付く面の構成頂点INDEXがdress_vertex_indexesの何番目に存在しているかのリスト
+        dress_near_vertices: dict[int, list[int]] = {}
+        for i, dress_face_index in enumerate(dress_face_indexes):
+            logger.count("衣装: 近似頂点選別", i, len(dress_face_indexes), display_block=100)
+
+            for vertex_index in dress.faces[dress_face_index].vertices:
+                if vertex_index not in dress_near_vertices:
+                    dress_near_vertices[vertex_index] = []
+                for near_vertex_index in dress.faces[dress_face_index].vertices:
+                    if near_vertex_index in dress_vertex_indexes:
+                        dress_near_vertices[vertex_index].append(
+                            [vi for (vi, vidx) in enumerate(dress_vertex_indexes) if vidx == near_vertex_index][0]
+                        )
 
         # 人物の指定材質に割り当てられた頂点INDEXが配置されている3次元頂点の位置
         model_vertex_positions = MVectorDict()
@@ -984,42 +1022,76 @@ class SaveUsecase:
         for dress_vertex_index in dress_vertex_indexes:
             dress_vertex_positions.append(dress_vertex_index, dress.vertices[dress_vertex_index].position)
 
-        # 衣装の指定材質に割り当てられた頂点INDEXが配置されている3次元頂点の位置に最も近い人物頂点の色
-        model_vertex_colors: list[np.ndarray] = []
-        dress_vertex_colors: list[np.ndarray] = []
+        # 衣装の指定材質に割り当てられた頂点INDEXが配置されている3次元頂点の位置に最も近い人物頂点を見つける
+        model_vertex_uv_list: list[np.ndarray] = []
+        dress_vertex_uv_list: list[np.ndarray] = []
+
+        # 面を構成している頂点（近似頂点）のリスト
+        model_vertices_by_face: list[list[int]] = []
+        dress_vertices_by_face: list[list[int]] = []
+
+        # 材質に割り当てられたテクスチャの色のRGBだけを取り出す
+        model_image_colors = np.copy(model_image[..., :3])
+        dress_image_colors = np.copy(dress_image[..., :3])
+
+        logger.info("近似テクスチャUV取得")
 
         for i, dress_vertex_index in enumerate(dress_vertex_indexes):
-            logger.count("近似テクスチャ色取得", i, len(dress_vertex_indexes), display_block=500)
+            logger.count("近似テクスチャUV取得", i, len(dress_vertex_indexes), display_block=500)
 
             nearest_model_vertex_index = model_vertex_positions.nearest_key(dress.vertices[dress_vertex_index].position)
             nearest_model_vertex = model.vertices[nearest_model_vertex_index]
             # 人物の指定頂点に割り当てられたテクスチャとUVから、テクスチャの該当位置を取得する
-            model_vertex_colors.append(
-                model_image[
-                    max(0, min((1 - int(nearest_model_vertex.uv.y) * model_image.shape[0]), model_image.shape[0] - 1)),
-                    max(0, min(int(nearest_model_vertex.uv.x * model_image.shape[1]), model_image.shape[1] - 1)),
-                    :3,
-                ]
+            model_vertex_uv_list.append(
+                np.array([int(nearest_model_vertex.uv.x * model_image.shape[0]), int(nearest_model_vertex.uv.y * model_image.shape[1])])
             )
+            # 面を構成している頂点（近似頂点）を求めておく
+            model_vertices_by_face.append(model_near_vertices[nearest_model_vertex_index])
 
             # 衣装の指定頂点に割り当てられたテクスチャとUVから、テクスチャの該当位置を取得する
             dress_vertex = dress.vertices[dress_vertex_index]
-            dress_vertex_colors.append(
-                dress_image[
-                    max(0, min((1 - int(dress_vertex.uv.y) * dress_image.shape[0]), dress_image.shape[0] - 1)),
-                    max(0, min(int(dress_vertex.uv.x * dress_image.shape[1]), dress_image.shape[1] - 1)),
-                    :3,
-                ]
+            dress_vertex_uv_list.append(
+                np.array([int(dress_vertex.uv.x * dress_image.shape[0]), int(dress_vertex.uv.y * dress_image.shape[1])])
             )
+            dress_vertices_by_face.append(dress_near_vertices[dress_vertex_index])
 
-        model_median_color = np.median(model_vertex_colors, axis=0)
-        dress_median_color = np.median(dress_vertex_colors, axis=0)
+        model_vertex_uvs = np.array(model_vertex_uv_list)
+        dress_vertex_uvs = np.array(dress_vertex_uv_list)
 
-        color_difference = dress_median_color - model_median_color
+        filled_dress_image = np.zeros(dress_image.shape)
 
-        logger.info("肌テクスチャ色補正: {c}", c=np.round(color_difference, decimals=1))
+        logger.info("肌テクスチャ色補正")
 
-        corrected_dress_image[..., :3] += color_difference
+        for fidx, (model_near_vertices_by_face, dress_near_vertices_by_face) in enumerate(
+            zip(model_vertices_by_face, dress_vertices_by_face)
+        ):
+            logger.count("肌テクスチャ色補正", fidx, len(model_vertices_by_face), display_block=500)
+
+            model_near_vertex_uvs = model_vertex_uvs[np.array(model_near_vertices_by_face)]
+            model_near_vertex_uvs[:, 0] = np.clip(model_near_vertex_uvs[:, 0], 0, model_image_colors.shape[0] - 1)
+            model_near_vertex_uvs[:, 1] = np.clip(model_near_vertex_uvs[:, 1], 0, model_image_colors.shape[1] - 1)
+
+            model_vertex_mean_color = np.mean(model_image_colors[model_near_vertex_uvs[:, 0], model_near_vertex_uvs[:, 1], :], axis=0)
+
+            dress_near_vertex_uvs = dress_vertex_uvs[np.array(dress_near_vertices_by_face)]
+            dress_near_vertex_uvs[:, 0] = np.clip(dress_near_vertex_uvs[:, 0], 0, dress_image_colors.shape[0] - 1)
+            dress_near_vertex_uvs[:, 1] = np.clip(dress_near_vertex_uvs[:, 1], 0, dress_image_colors.shape[1] - 1)
+
+            dress_vertex_mean_color = np.mean(dress_image_colors[dress_near_vertex_uvs[:, 0], dress_near_vertex_uvs[:, 1], :], axis=0)
+
+            dress_min_uv = np.min(dress_near_vertex_uvs, axis=0)
+            dress_max_uv = np.max(dress_near_vertex_uvs, axis=0)
+
+            # 人物と衣装の該当UV位置にある色の差分を取得する
+            color_difference = model_vertex_mean_color - dress_vertex_mean_color
+
+            # テクスチャの処理対象領域内でまだ補正されていない箇所だけ補正する
+            for u, v in product(
+                [u for u in range(dress_min_uv[1], (dress_max_uv[1] + 1))], [v for v in range(dress_min_uv[0], (dress_max_uv[0] + 1))]
+            ):
+                if np.sum(filled_dress_image[u, v]) == 0:
+                    corrected_dress_image[u, v, :3] += color_difference
+                    filled_dress_image[u, v, :3] = np.where(color_difference == 0, 1, color_difference)
 
         # 補正後の色が0未満または255を超える場合、範囲内にクリップする
         corrected_dress_image = np.clip(corrected_dress_image, 0, 255)
